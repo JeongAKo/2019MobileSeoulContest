08e86d4fa4359c60eef29f9cfa42a67d8f28e4cd
Chang-Geun-Ryu <goodrcg@gmail.com>
2019-08-30 13:57:10 +0900

    add: pod install

---
+++ MountainTop/MountainTop/VC/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  MountainTop
+//
+//  Created by CHANGGUEN YU on 28/08/2019.
+//  Copyright © 2019 CHANGGUEN YU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  private lazy var loginButton: UIButton = {
+    let btn = UIButton(type: .custom)
+    btn.setTitle("kakao login", for: .normal)
+    btn.backgroundColor = .yellow
+    return btn
+  }()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+  }
+  
+  private func autolayout 
+  
+  private func configureView() {
+    
+  }
+}
+
