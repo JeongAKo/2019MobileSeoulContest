1b2b4d554206f400d3dfffda63c6b2bcc53ae8bb
Chang-Geun-Ryu <goodrcg@gmail.com>
2019-09-04 20:46:14 +0900

    kakao login

--- MountainTop/MountainTop/AppDelegate.swift
+++ MountainTop/MountainTop/AppDelegate.swift
@@ -13,39 +13,87 @@ import Firebase
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
+  var loginViewController: UIViewController?
+  var mainViewController: UIViewController?
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.backgroundColor = .white
     
     // Firebase 등록
     FirebaseApp.configure()
     
+    setupEntryController()
+    
     // Kakao
-    initializeKakao()
+    NotificationCenter.default.addObserver(self,
+                                           selector: #selector(AppDelegate.kakaoSessionDidChangeWithNotification),
+                                           name: NSNotification.Name.KOSessionDidChange,
+                                           object: nil)
     
-    window = UIWindow(frame: UIScreen.main.bounds)
+    reloadRootViewController()
     
-    window?.backgroundColor = .white
-    window?.makeKeyAndVisible()
-    window?.rootViewController = LoginVC()
     
     return true
   }
   
-  func applicationDidEnterBackground(_ application: UIApplication) {
-    KOSession.handleDidEnterBackground() // Kakao 서버에 상태 전달
-  }
-  
-  func applicationDidBecomeActive(_ application: UIApplication) {
-    KOSession.handleDidBecomeActive() // Kakao 서버에 상태 전달
-  }
-  
-  func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+  fileprivate func setupEntryController() {
+//    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+    let navigationController = UINavigationController(rootViewController: LoginVC())
+    let navigationController2 = UINavigationController(rootViewController: LogoutVC())
     
+//    let viewController = storyboard.instantiateViewController(withIdentifier: "login") as UIViewController
+//    navigationController.pushViewController(viewController, animated: true)
+    self.loginViewController = navigationController
+    
+//    let viewController2 = storyboard.instantiateViewController(withIdentifier: "main") as UIViewController
+//    navigationController2.pushViewController(viewController2, animated: true)
+    self.mainViewController = navigationController2
+  }
+  
+  fileprivate func reloadRootViewController() {
+    guard let isOpened = KOSession.shared()?.isOpen() else {
+      print("isOpened is nil")
+      return
+    }
+    if !isOpened {
+      let mainViewController = self.mainViewController as! UINavigationController
+      mainViewController.popToRootViewController(animated: true)
+    }
+    
+    self.window?.rootViewController = isOpened ? self.mainViewController : self.loginViewController
+    self.window?.makeKeyAndVisible()
+  }
+  
+  @objc func kakaoSessionDidChangeWithNotification() {
+    reloadRootViewController()
+  }
+  
+  func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+    if KOSession.handleOpen(url) {
+      return true
+    }
     return false
   }
   
-  func initializeKakao() {
-    
+  func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+    if KOSession.handleOpen(url) {
+      return true
+    }
+    return false
+  }
+  
+  func applicationDidEnterBackground(_ application: UIApplication) {
+    KOSession.handleDidEnterBackground()
+  }
+  
+  func applicationDidBecomeActive(_ application: UIApplication) {
+    KOSession.handleDidBecomeActive()
+  }
+  
+  func applicationDidReceiveMemoryWarning(_ application: UIApplication) {
+//    MapleBaconStorage.sharedStorage.clearMemoryStorage()
   }
 }
 
