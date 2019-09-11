c91384dca70963392fb65487cc18ac06a818d414
Chang-Geun-Ryu <goodrcg@gmail.com>
2019-09-11 17:51:53 +0900

    feat: profile

---
+++ MountainTop/MountainTop/Extensions/TableCollectionExtension.swift
@@ -0,0 +1,9 @@
+//
+//  TableCollectionExtension.swift
+//  MountainTop
+//
+//  Created by CHANGGUEN YU on 11/09/2019.
+//  Copyright © 2019 CHANGGUEN YU. All rights reserved.
+//
+
+import Foundation
---
+++ MountainTop/MountainTop/VC/Profile/Cell/DefaultCell.swift
@@ -0,0 +1,24 @@
+//
+//  DefaultCell.swift
+//  MountainTop
+//
+//  Created by CHANGGUEN YU on 11/09/2019.
+//  Copyright © 2019 CHANGGUEN YU. All rights reserved.
+//
+
+import UIKit
+
+class DefaultCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
