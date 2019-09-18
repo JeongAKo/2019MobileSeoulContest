6985c29c7a3313e8a8ffc9b19cd70e152fe15b6f
JeongAKo <kja2683@gmail.com>
2019-09-18 15:05:55 +0900

    Add: Timinterval

---
+++ MountainTop/MountainTop/Extensions/TimeInterval.swift
@@ -0,0 +1,20 @@
+//
+//  TimeInterval.swift
+//  MountainTop
+//
+//  Created by Daisy on 17/09/2019.
+//  Copyright © 2019 CHANGGUEN YU. All rights reserved.
+//
+
+import Foundation
+
+extension TimeInterval {
+  func asTimeString() -> String {
+    
+    let hours = Int(self) / 3600
+    let minutes = Int(self) / 60 % 60
+    let seconds = Int(self) % 60
+    
+    return String(format: "%02i:%02i:%02i", hours, minutes, seconds  )
+  }
+}
--- MountainTop/MountainTop/VC/NMapVC.swift
+++ MountainTop/MountainTop/VC/NMapVC.swift
@@ -13,20 +13,37 @@ import NMapsMap
 class NMapVC: UIViewController, NMFMapViewDelegate {
   
   private let activityIndicator = UIActivityIndicatorView(style: .gray)
-  private let containerView = UIView()
+  private let recordView = RecordTopView()
+  private let calender = Calendar.current
+  private var timer = Timer()
+  private lazy var startDate = Date()
   
-  let recordView = RecordTopView()
-  let calender = Calendar.current
+  private lazy var recordContainerView: UIView = {
+    let rView = UIView()
+    rView.layer.shadowColor = UIColor.lightGray.cgColor
+    rView.layer.shadowOffset = CGSize.zero
+    rView.layer.shadowRadius = 10
+    rView.layer.shadowOpacity = 0.5
+    return rView
+  }()
   
-  var timer = Timer()
-  lazy var startDate = Date()
+  private lazy var buttonContainerView: UIView = {
+    let bView = UIView()
+    bView.layer.shadowColor = UIColor.lightGray.cgColor
+    bView.layer.shadowOffset = CGSize.zero
+    bView.layer.shadowRadius = 10
+    bView.layer.shadowOpacity = 0.5
+    return bView
+  }()
   
   private lazy var cameraButton: UIButton = {
     let button = UIButton(type: .system)
     button.setTitle("사진찍기", for: .normal)
-    button.backgroundColor = .blue
+    button.setTitleColor(.white, for: .normal)
+    button.titleLabel?.font = UIFont.init(name: "Helvetica Bold Oblique", size: 15)
+    button.backgroundColor = .black
+    button.alpha = 0.5
     button.addTarget(self, action: #selector(didTapCameraButton(_:)), for: .touchUpInside)
-    view.addSubview(button)
     return button
   }()
   
@@ -40,7 +57,7 @@ class NMapVC: UIViewController, NMFMapViewDelegate {
   private var mapLocation: NMFLocationOverlay!
   
   private var location: CLLocationManager!
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
     
@@ -48,11 +65,9 @@ class NMapVC: UIViewController, NMFMapViewDelegate {
     
     mapLocation = naverMapView.mapView.locationOverlay
     
-    view.addSubview(naverMapView)
-    view.addSubview(containerView)
-    containerView.addSubview(recordView)
+    addsubViews(naverMapView)
+    
     configureMapView(naverMapView)
-    applyDesign()
     makeConstraints()
     
     location = CLLocationManager()
@@ -60,10 +75,38 @@ class NMapVC: UIViewController, NMFMapViewDelegate {
     location.distanceFilter = 10_000.0
     location.delegate = self
     location.startUpdatingLocation()
+    
+//    locationOfMarker()
+    
+    let marker: NMFMarker = {
+      let marker = NMFMarker()
+      marker.position = NMGLatLng(lat: 37.5670135, lng: 126.9783740)
+      marker.iconImage = NMFOverlayImage(name: "flag")
+      marker.mapView = naverMapView.mapView
+      return marker
+    }()
   }
   
-  private func configureMapView(_ naverMapView: NMFNaverMapView) {
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    applyDesign()
+  }
+  
+  // FIXME: - 옮겨보자
+  
+  private func locationOfMarker() {
+//  let marker: NMFMarker = {
+//    let marker = NMFMarker()
+//    marker.position = NMGLatLng(lat: 37.686099, lng: 127.036238)
+//    marker.anchor = CGPoint(x: 1, y: 1)
+//    marker.mapView = self.naverMapView.mapView
+//    return marker
+//  }()
     
+  }
+  
+  // MARK: - Action method
+  private func configureMapView(_ naverMapView: NMFNaverMapView) {
     naverMapView.mapView.setLayerGroup(NMF_LAYER_GROUP_MOUNTAIN, isEnabled: true)  // 등산로 모드
     naverMapView.positionMode = .direction
     naverMapView.showLocationButton = true   // 현 위치 버튼이 활성화되어 있는지 여부
@@ -75,39 +118,20 @@ class NMapVC: UIViewController, NMFMapViewDelegate {
      */
   }
   
-  func time() {
+  private func time() {
     timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(keepTimer), userInfo: nil, repeats: true)
     
   }
   
-  @objc func keepTimer() {
-    
-    let startTime = calender.dateInterval(of: .nanosecond, for: startDate)
-    var endDate = Date()
-    let endTime = calender.dateInterval(of: .nanosecond, for: endDate)
-    let timePeriod = calender.dateComponents([.second], from: startTime!.start, to: endTime!.end)
-    let progressTime = timePeriod.second
-    
-    recordView.challengerRecordTimeLabel.text = "\(progressTime)"
-    
-    let dateFormatter = DateFormatter()
-    dateFormatter.locale = Locale(identifier: "ko_KR")
-    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-    
-    let endFommat = dateFormatter.string(from: endDate)
-    let startFommat = dateFormatter.string(from: startDate)
-
-    print("endFommat", endFommat)
-    print("startFommat", startFommat)
-    print("timePeriod", timePeriod)
-    print("progressTime", progressTime)
-  }
-
-  @objc func didTapCameraButton(_ sender: UIButton) {
-    present(imagePickerController, animated: true)
+  fileprivate func addsubViews(_ naverMapView: NMFNaverMapView) {
+    view.addSubview(naverMapView)
+    view.addSubview(recordContainerView)
+    view.addSubview(buttonContainerView)
+    recordContainerView.addSubview(recordView)
+    buttonContainerView.addSubview(cameraButton)
   }
   
-  func presentAlert(title: String, message: String) {
+  private func presentAlert(title: String, message: String) {
     print("✏️ presentAlert")
     activityIndicator.stopAnimating()
     let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
@@ -115,35 +139,63 @@ class NMapVC: UIViewController, NMFMapViewDelegate {
     present(alert, animated: true)
   }
   
-func saveToAlbum(named: String, image: UIImage) {
-  print("📍saveToAlbum")
-  let album = CustomAlbum(name: named)
-  album.save(image: image) { (result) in
-  DispatchQueue.main.async {
-    switch result {
-    case .success(_):
-      self.presentAlert(title: "사진 저장", message: "사진이\(named) 앨범에 저장 되었습니다.")
-    case .failure(let err):
-      self.presentAlert(title: "Error", message: err.localizedDescription)
+  private func saveToAlbum(named: String, image: UIImage) {
+    print("📍saveToAlbum")
+    let album = CustomAlbum(name: named)
+    album.save(image: image) { (result) in
+      DispatchQueue.main.async {
+        switch result {
+        case .success(_):
+          self.presentAlert(title: "사진 저장", message: "사진이\(named) 앨범에 저장 되었습니다.")
+        case .failure(let err):
+          self.presentAlert(title: "Error", message: err.localizedDescription)
         }
       }
     }
   }
   
   private func applyDesign() {
-    let cornerRadius : CGFloat = 15.0
+//    let cornerRadius : CGFloat = 15.0
+    view.layoutIfNeeded()
     
-    recordView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner] //하단만 cornerRadius 적용
-    recordView.layer.cornerRadius = cornerRadius
+    let recordVCorner = recordContainerView.frame.height / 5
+    recordView.layer.cornerRadius = recordVCorner
     recordView.clipsToBounds = true
     
-    containerView.layer.shadowColor = UIColor.darkGray.cgColor
-    containerView.layer.shadowOffset = CGSize.zero
-    containerView.layer.shadowRadius = 10
-    containerView.layer.shadowOpacity = 0.5
-    
+    let cameraButtonCorner = cameraButton.frame.height / 5
+    cameraButton.layer.cornerRadius = cameraButtonCorner
+    cameraButton.clipsToBounds = true
   }
   
+  @objc private func keepTimer() {
+    
+    let startTime = calender.dateInterval(of: .nanosecond, for: startDate)
+    var endDate = Date()
+    let endTime = calender.dateInterval(of: .nanosecond, for: endDate)
+    let timePeriod = calender.dateComponents([.second], from: startTime!.start, to: endTime!.end)
+    let progressTime = timePeriod.second ?? 0
+    
+    //    let duration = TimeInterval(Double(progressTime ?? 0))
+    
+    let duration = TimeInterval(progressTime)
+    recordView.challengerRecordTimeLabel.text = duration.asTimeString() // Duration -> Date
+    
+    let dateFormatter = DateFormatter()
+    dateFormatter.locale = Locale(identifier: "ko_KR")
+    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+    
+    let endFommat = dateFormatter.string(from: endDate)
+    let startFommat = dateFormatter.string(from: startDate)
+    
+    print("endFommat", endFommat)
+    print("startFommat", startFommat)
+    print("timePeriod", timePeriod)
+    print("progressTime", progressTime)
+  }
+  
+  @objc func didTapCameraButton(_ sender: UIButton) {
+    present(imagePickerController, animated: true)
+  }
   
   @objc private func presentCamera(_ sender: Notification) {
     
@@ -156,7 +208,6 @@ func saveToAlbum(named: String, image: UIImage) {
     print("location lat: \(mapLocation.location.lat), location lng: \(mapLocation.location.lng)")
     print("location.location?.altitude: \(String(describing: location.location?.altitude))")
     present(picker, animated: true)
-  
   }
   
   @objc private func presentAlert(_ sender: Notification) {
@@ -166,16 +217,15 @@ func saveToAlbum(named: String, image: UIImage) {
       else {
         return print("fail downCasting")
     }
-  
+    
     present(alert, animated: true)
-  
   }
   
   // MARK: - AutoLayout
   private func makeConstraints() {
-    containerView.snp.makeConstraints {
-      $0.top.leading.trailing.equalToSuperview()
-      $0.height.equalToSuperview().multipliedBy(0.13)
+    recordContainerView.snp.makeConstraints {
+      $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
+      $0.height.equalToSuperview().multipliedBy(0.1)
     }
     
     recordView.snp.makeConstraints {
@@ -183,11 +233,16 @@ func saveToAlbum(named: String, image: UIImage) {
       $0.height.equalToSuperview()
     }
     
-    cameraButton.snp.makeConstraints {
+    buttonContainerView.snp.makeConstraints {
       $0.centerX.equalToSuperview()
       $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(50)
       $0.width.equalToSuperview().multipliedBy(0.6)
       $0.height.equalToSuperview().multipliedBy(0.05)
+    }
+    
+    cameraButton.snp.makeConstraints {
+      $0.top.leading.trailing.equalToSuperview()
+      $0.height.equalToSuperview()
     }
   }
 }
@@ -207,9 +262,9 @@ extension NMapVC: CLLocationManagerDelegate {
   
   func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
     
-//    locations.first?.altitude
+    //    locations.first?.altitude
     
-//    print("altitude", locations.first?.altitude)
+    //    print("altitude", locations.first?.altitude)
     
   }
   
@@ -231,6 +286,6 @@ extension NMapVC: UINavigationControllerDelegate, UIImagePickerControllerDelegat
     print("📷saved image")
     time()
     imagePickerController.dismiss(animated: true, completion: nil)
-    
   }
 }
+
