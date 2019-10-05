8aba81e0b27e21fda385f6c0501954246bdc0ee7
Chang-Geun-Ryu <goodrcg@gmail.com>
2019-10-05 15:50:17 +0900

    feat: add policy

---
+++ MountainTop/MountainTop/VC/NMapVC/FinishTestVC.swift
@@ -0,0 +1,35 @@
+//
+//  FinishTestVC.swift
+//  MountainTop
+//
+//  Created by CHANGGUEN YU on 28/09/2019.
+//  Copyright © 2019 CHANGGUEN YU. All rights reserved.
+//
+
+import UIKit
+
+class FinishTestVC: UIViewController {
+  
+//  public lazy var mainLabel: UILabel = {
+//    let label = UILabel(frame: .zero)
+//    label.text = "우왕!!!!!"
+//    label.font = UIFont.systemFont(ofSize: 25, weight: .semibold)
+//    self.view.addSubview(label)
+//    return label
+//  }()
+//
+//  override func viewDidLoad() {
+//    super.viewDidLoad()
+//    view.backgroundColor = .white
+//    setupLayout()
+//  }
+//
+//  private func setupLayout() {
+//
+//    mainLabel.snp.makeConstraints {
+//      $0.top.leading.equalToSuperview().inset(Metric.margin)
+//    }
+//  }
+
+  
+}
